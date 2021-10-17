01d054695e5645e59be3e3e2394ca01c334716be
alexgershberg <62508229+0x6f6f66@users.noreply.github.com>
2021-10-17 21:24:03 +0100

    Add server side commands

--- client.py
+++ client.py
@@ -1,15 +1,8 @@
 import socket
 import threading
 
-host = '0.0.0.0'
-port = 10004
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
-
 
 def receive():
-    print("[rThread - receive] start")
     while True:
         try:
             message = (client.recv(1024)).decode('ascii')
@@ -17,27 +10,36 @@ def receive():
                 client.send(nickname.encode('ascii'))
             else:
                 print(message)
-        except:
+        # If something weird gets sent the client.recv method
+        except BrokenPipeError:
             print("Error occurred!")
             client.close()
             break
-    print("[rThread - receive] end")
+        # If we stop the server while client is running
+        except ConnectionResetError:
+            print("Server has closed.")
+            client.close()
+            break
 
 
 def write():
-    print("[wThread - write] start")
     while True:
         message = f'{nickname}: {input("")}'
         client.send(message.encode('ascii'))
 
 
 if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = 10004
+    server = (host, port)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(server)
+
     nickname = input("choose a nickname: ")
 
-    print("rThread")
     rThread = threading.Thread(target=receive)
     rThread.start()
 
-    print("wThread")
     wThread = threading.Thread(target=write)
     wThread.start()
--- server.py
+++ server.py
@@ -1,55 +1,111 @@
 import socket
 import threading
 
-host = '0.0.0.0'  # localhost
-port = 10004
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
-
-clients = []
-nicknames = []
-
 
 def broadcast(message):
-    for client in clients:
-        client.send(message.encode('ascii'))
+    for address in clients:
+        try:
+            clients[address]['client'].send(message.encode('ascii'))
+        except BrokenPipeError:
+            clients[address]['client'].close()
+            clients.pop(address, None)
 
 
-def handle(client):
+def handle(client: socket.socket):
     while True:
         try:
             message = (client.recv(1024)).decode('ascii')
             broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast(f"[HANDLE] {nickname} has left the chat.")
-            nicknames.remove(nickname)
+        except BrokenPipeError:
+            clients.pop(address, None)
             break
 
 
-def receive():
+def receive(client: socket.socket, address: (str, int)):
+    print(f"[SERVER] {address} connected to the server")
+
+    # tmp
+    print(f"[SERVER] client: {client}")
+
+    clients[address] = {
+        'nickname': None,
+        'address': address,
+        'client': client
+    }
+
+    try:
+        clients[address]['client'].send('NICK'.encode('ascii'))
+        nickname = (clients[address]['client'].recv(1024)).decode('ascii')
+        clients[address]['nickname'] = nickname
+    except BrokenPipeError:
+        clients[address]['client'].send(f'An error has occurred!'.encode('ascii'))
+        clients[address]['client'].close()
+        clients.pop(address, None)
+
+    # tmp
+    for cl in clients:
+        print(f"[SERVER] {cl}: {clients[cl]}")
+
+    print(f"[SERVER] {address} has set their nickname to {clients[address]['nickname']}")
+    clients[address]['client'].send("You have successfully connected to the server.".encode('ascii'))
+    broadcast(f"{clients[address]['nickname']} joined the server!")
+
+    handle(client)
+
+
+def execute_commands():
+    def get_help():
+        help_instructions = \
+            "help      : get list of possible commands\n" \
+            "online    : see who is online\n" \
+            "nicknames : get client's address and their corresponding nickname"
+        return help_instructions
+
+    def get_online():
+        return clients
+
+    def get_nicknames():
+        nicknames = []
+        for address in clients:
+            nicknames.append((address, clients[address]['nickname']))
+        return nicknames
+
+    def default():
+        return f"command not found: {command}"
+
     while True:
-        client, address = server.accept()
-        print(f"[SERVER] {address} connected to the server")
+        command_raw = str(input())
+        if not command_raw:
+            command = ''
+        else:
+            command = command_raw.split()[0]
 
-        client.send('NICK'.encode('ascii'))
-        nickname = (client.recv(1024)).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
+        commands = {
+            'help': get_help,
+            'online': get_online,
+            'nicknames': get_nicknames
+        }
 
-        print(f"[SERVER] {address} has set their nickname to {nickname}")
-        broadcast(f"{nickname} joined the server!")
-        client.send("You have successfully connected to the server.".encode('ascii'))
-
-        cThread = threading.Thread(target=handle, args=(client,))
-        cThread.start()
+        print(commands.get(command, default)())
 
 
 if __name__ == '__main__':
+    host = '0.0.0.0' # localhost
+    port = 10004
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+
+    clients = {}
+
+    execThread = threading.Thread(target=execute_commands)
+    execThread.start()
+
     print("Listening to connections...")
-    receive()
+
+    while True:
+        client, address = server.accept()
+
+        recThread = threading.Thread(target=receive, args=(client, address))
+        recThread.start()
